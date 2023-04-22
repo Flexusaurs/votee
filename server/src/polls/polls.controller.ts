91a04dbaa8e584b8f7b4c912d7c422196bd9c63b
Itay Flexusaurs <eitayhaim@gmail.com>
2023-04-22 19:27:35 +0300

    added post routes

--- server/src/polls/polls.controller.ts
+++ server/src/polls/polls.controller.ts
@@ -1,22 +1,26 @@
-import { Controller, Post, Logger } from '@nestjs/common';
+import { Controller, Post, Logger, Body } from '@nestjs/common';
+import { CreatePollDto, JoinPollDto } from './dtos';
 
 @Controller('polls')
 export class PollsController{
 
     @Post()
-    async create(){
+    async create(@Body() createPollDto: CreatePollDto){
         Logger.log('in creation');
+        return createPollDto;
     }
 
     
     @Post('/join')
-    async join(){
+    async join(@Body() joinPollDto: JoinPollDto){
         Logger.log('joining');
+        return joinPollDto;
     }
 
     @Post('/rejoin')
-    async rejoin(){
+    async rejoin(@Body() joinPollDto: JoinPollDto){
         Logger.log('rejoining');
+        return joinPollDto;
     }
     
 }

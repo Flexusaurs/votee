f7ac6ca8cf71171c770274679bc1692bdbfb799b
Itay Flexusaurs <eitayhaim@gmail.com>
2023-04-27 00:52:57 +0300

    added service and controlller integration

--- server/src/polls/polls.controller.ts
+++ server/src/polls/polls.controller.ts
@@ -1,26 +1,33 @@
 import { Controller, Post, Logger, Body } from '@nestjs/common';
 import { CreatePollDto, JoinPollDto } from './dtos';
+import { PollsService } from './polls.service';
 
 @Controller('polls')
-export class PollsController{
+export class PollsController {
+  constructor(private pollsService: PollsService) {}
 
-    @Post()
-    async create(@Body() createPollDto: CreatePollDto){
-        Logger.log('in creation');
-        return createPollDto;
-    }
+  @Post()
+  async create(@Body() createPollDto: CreatePollDto) {
+    const result = await this.pollsService.createPoll(createPollDto);
 
-    
-    @Post('/join')
-    async join(@Body() joinPollDto: JoinPollDto){
-        Logger.log('joining');
-        return joinPollDto;
-    }
+    return result;
+  }
 
-    @Post('/rejoin')
-    async rejoin(@Body() joinPollDto: JoinPollDto){
-        Logger.log('rejoining');
-        return joinPollDto;
-    }
-    
+  @Post('/join')
+  async join(@Body() joinPollDto: JoinPollDto) {
+    const result = await this.pollsService.joinPoll(joinPollDto);
+
+    return result;
+  }
+
+  @Post('/rejoin')
+  async rejoin() {
+    const result = await this.pollsService.rejoinPoll({
+      name: 'From token',
+      pollID: 'Also from token',
+      userID: 'Guess where this comes from?',
+    });
+
+    return result;
+  }
 }
